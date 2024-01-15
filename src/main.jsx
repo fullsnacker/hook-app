b2dcd34003bd9e1c559cd13c2e033cf9edf3d78f
fullsnacker <fullsnacker@gmail.com>
2024-01-15 11:40:26 -0300

    useRef hook

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import './index.css';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	// <CounterWithCustomHook/>
 	// <SimpleForm />
 	//  <FormWithCustomHook />
-	<MultipleCustomHooks />
+	// <MultipleCustomHooks />
+	<FocusScreen />
 	// </React.StrictMode>
 );
