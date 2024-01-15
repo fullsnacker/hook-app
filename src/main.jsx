975b45c96d3a87ee3847fb2692bc240ad3f458ef
fullsnacker <fullsnacker@gmail.com>
2024-01-15 12:37:24 -0300

    memo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Memorize } from './06-memos/Memorize';
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -8,7 +9,7 @@ import './index.css';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	//  <FormWithCustomHook />
 	// <MultipleCustomHooks />
 	// <FocusScreen />
-	<Layout />
+	// <Layout />
+	<Memorize />
 	// </React.StrictMode>
 );
