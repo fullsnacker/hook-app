49d3a6223083e1f288fe413e3c32f8260ef772f7
fullsnacker <fullsnacker@gmail.com>
2024-01-16 17:18:57 -0300

    Tarea Memorize

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import './index.css';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	// <FocusScreen />
 	// <Layout />
 	// <Memorize />
-	<MemoHook />
+	// <MemoHook />
+	<Padre />
 	// </React.StrictMode>
 );
